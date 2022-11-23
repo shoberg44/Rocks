13293402f562b30fb6fe5ef4a2f6d3ac084819e2
shoberg44 <steven.hoberg23@d155.org>
2022-11-22 18:10:50 -0600

    vs mode working

--- Rocks/VsViewController.swift
+++ Rocks/VsViewController.swift
@@ -9,14 +9,18 @@ import UIKit
 
 class VsViewController: UIViewController {
     
-    @IBOutlet weak var userSelectedImage: UIImageView!
-    @IBOutlet weak var AIdisplay: UILabel!
-    @IBOutlet weak var AISelectedImage: UIImageView!
+    @IBOutlet weak var selection1Icon: UIImageView!
     @IBOutlet weak var resultLabel: UILabel!
-    var score : Int = 0
-    var streak : Int = 0
-    var selection : selectionOptions = .unselected
-    var selectionAI : selectionOptions = .unselected
+    @IBOutlet weak var selection2Icon: UIImageView!
+    @IBOutlet weak var playerLabel: UILabel!
+    @IBOutlet weak var player1ScoreLabel: UILabel!
+    @IBOutlet weak var player2ScoreLabel: UILabel!
+    
+    var selectionP1 : selectionOptions = .unselected
+    var selectionP2 : selectionOptions = .unselected
+    var isFirstPlayer = true //keeps track of player turns
+    var scoreboard : (Int,Int) = (0,0) //keeps score of seesion
+    let multipler = 5 //default 1
     
     enum selectionOptions : String{
         case rock = "Rock"
@@ -25,27 +29,75 @@ class VsViewController: UIViewController {
         case unselected = "Unselected"//unused
     }
     
-    
+    //
+
     @IBAction func rockButton(_ sender: UIButton) {
-        selection = selectionOptions.rock
+        if isFirstPlayer{
+            selectionP1 = selectionOptions.rock
+            isFirstPlayer = false
+            phaseUpdate()
+        }
+        else{
+            selectionP2 = selectionOptions.rock
+            convertVS()
+        }
         //UIImage.Orientation = .upMirrored
-        userSelectedImage.image = UIImage(named: "rockPic")
-        begin()
     }
     @IBAction func scissorsButton(_ sender: UIButton) {
-        selection = selectionOptions.scissors
-        userSelectedImage.image = UIImage(named: "scissorsPic")
-        begin()
+        if isFirstPlayer{
+            selectionP1 = selectionOptions.scissors
+            isFirstPlayer = false
+            phaseUpdate()
+        }
+        else{
+            selectionP2 = selectionOptions.scissors
+            convertVS()
+        }
     }
     @IBAction func paperButton(_ sender: UIButton) {
-        selection = selectionOptions.paper
-        userSelectedImage.image = UIImage(named: "paperPic")
-        begin()
+        if isFirstPlayer{
+            selectionP1 = selectionOptions.paper
+            isFirstPlayer = false
+            phaseUpdate()
+        }
+        else{
+            selectionP2 = selectionOptions.paper
+            convertVS()
+        }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
+    func convertVS(){
+        if AppData.result(selectionP1.rawValue, selectionP2.rawValue) == "You Win"{
+            resultLabel.text = "Player 1 Wins!"
+            scoreboard.0 += (1 * multipler)
+        }
+        else if AppData.result(selectionP1.rawValue, selectionP2.rawValue) == "You Lose"{
+            resultLabel.text = "Player 2 Wins!"
+            scoreboard.1 += (1 * multipler)
+        }
+        else{
+            resultLabel.text = AppData.result(selectionP1.rawValue, selectionP2.rawValue)
+        }
+        isFirstPlayer = true
+        phaseUpdate()
+        updateScore()
+    }
+    func phaseUpdate(){
+        if isFirstPlayer{
+            selection1Icon.isHidden = false
+            selection2Icon.isHidden = true
+            playerLabel.text = "Player 1"
+        }
+        else{
+            selection1Icon.isHidden = true
+            selection2Icon.isHidden = false
+            playerLabel.text = "Player 2"
+        }
+    }
+    func updateScore(){
+        player1ScoreLabel.text = String(scoreboard.0)
+        player2ScoreLabel.text = String(scoreboard.1)
+    }
 }
